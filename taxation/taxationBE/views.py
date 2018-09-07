58240fd7864a64795d0c37b376a0674abe747e34
github username <github email address>
2018-09-07 11:25:27 -0400

    posting needs work

--- taxation/taxationBE/views.py
+++ taxation/taxationBE/views.py
@@ -1,5 +1,8 @@
 from .models import UserInfo, State, Federal
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import UserInfoSerializer, StateSerializer, FederalSerializer
 
 class UserInfoViewSet(viewsets.ModelViewSet):
@@ -13,4 +16,19 @@ class StateViewSet(viewsets.ModelViewSet):
 class FederalViewSet(viewsets.ModelViewSet):
     queryset = Federal.objects.all()
     serializer_class = FederalSerializer
-#commit test
+
+# @api_view(['GET', 'POST'])
+#
+# def UserInfoList(request):
+#
+#     if request.method == 'GET':
+#         queryset = UserInfo.objects.all()
+#         serializer = UserInfoSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         serializer = UserInfoSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
